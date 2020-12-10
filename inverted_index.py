eb031ab58082841998bbec2dd5d26584966ff9df
longyyu <longyyu@umich.edu>
2020-12-11 00:32:07 +0800

    Update Indexes class: seperate tokenization and computing doc_freq from generate inverted index; Add function 'get_retrieval_results'; Use pickle to read and store dictionaries

--- inverted_index.py
+++ inverted_index.py
@@ -3,13 +3,16 @@ from nltk.stem import PorterStemmer
 porter = PorterStemmer()
 import pandas as pd
 import numpy as np
+import math
 import time
+import pickle
+import os
 
 from data_prep import script_utterance
 
 # Purpose: This script #! add header
 # Author: Yanyu Long
-# Updated: Dec 10, 2020
+# Updated: Dec 11, 2020
 
 # Define a decorator to measure the execution times of dedicated methods
 def measure_time(f):
@@ -23,91 +26,167 @@ def measure_time(f):
     return result
   return timed
 
+@measure_time
+def read_dict(file_path):
+  with open(file_path, 'rb') as f:
+    obj = pickle.load(f)
+  return(obj)
+
+@measure_time
+def save_dict(obj, file_path):
+  with open(file_path, 'wb') as f:
+    pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
 class Indexes:
-  def __init__(self, documents, stop_words, stem = False):
+  def __init__(self, documents, stop_words, doc_id = None, stem = False):
+    self.stop_list = stop_words # a list of stop words
+    self.do_stem = stem # whether to stem the terms when tokenizing
     self.documents = documents
     self.doc_count = len(documents)
-    self.dict_df = None
-    self.posting_df = None
-    self.stop_list = stop_words
-    # whether to stem the terms when tokenizing
-    self.do_stem = stem
+    self.doc_id = range(0, self.doc_count) if doc_id is None else doc_id
+    self.doc_length = dict(zip(self.doc_id, [len(doc) for doc in documents]))
+    self.avg_doc_length = np.mean(list(self.doc_length.values()))
+    
+    # self.doc_tokens: a dictionary that maps a document's ID to its tokens
+    self.doc_tokens = read_dict("./data/doc_tokens.pkl") \
+      if os.path.exists("./data/doc_tokens.pkl") else None
+    if self.doc_tokens is None:
+      self.tokenize_all_documents()
+    
+    # self.term_to_freq_pos: a dictionary that maps a tuple of (doc_id, term)
+    # to a list of [term frequency (an integer), position (a integer list)]
+    self.term_to_freq_pos = read_dict("./data/term_to_freq_pos.pkl") \
+      if os.path.exists("./data/term_to_freq_pos.pkl") else None
+    if self.term_to_freq_pos is None:
+      self.generate_inverted_index()
+    
+    # self.doc_freq: a dictionary that maps term to its document frequency
+    self.doc_freq = None
+    self.compute_doc_freq()
 
-  def tokenize(self, document):
+  def tokenize(self, document, remove_stop_words = False):
     # tokenize the given document
-    # input: document - a string, the original document
-    # output: a list of strings, terms from the original document
+    # inputs: 
+    #   document - string, the original document
+    #   remove_stop_words - boolean, whether to remove stop words from tokens
+    # output: list of strings, terms from the original document
     #         stemmed if self.do_stem is True
-    doc_tokens = word_tokenize(document.strip().lower())  
+    doc_tokens = word_tokenize(document.strip().lower())
+    if remove_stop_words:
+      doc_tokens = [term for term in doc_tokens if term not in self.stop_list]
     if self.do_stem:
       doc_tokens = [porter.stem(term) for term in doc_tokens]
     else:
       doc_tokens = [term for term in doc_tokens]
     return(doc_tokens)
+  
+  @measure_time
+  def tokenize_all_documents(self):
+    doc_tokens_list = []
+    for doc in self.documents:
+      doc_tokens_list.append(self.tokenize(doc))
+    # update self.doc_tokens and save to disk
+    self.doc_tokens = dict(zip(self.doc_id, doc_tokens_list))
+    save_dict(self.doc_tokens, "./data/doc_tokens.pkl")
 
   @measure_time
   def generate_inverted_index(self):
-    term_to_freq_pos_dict = dict()
-
-    for doc_id, doc in enumerate(self.documents):
-      if doc_id % 2000 == 0:
-        print(f"    Processing document # {doc_id} ...")
-      
-      # tokenize the document
-      doc_tokens = self.tokenize(doc)
-      
-      # update the posting
-      for pos, term in enumerate(doc_tokens):
+    self.term_to_freq_pos = dict()
+    for idx, doc_id in enumerate(self.documents):
+      # print processing status every 2000 documents
+      if idx % 2000 == 0:
+        print("    Processing document # {:5d} (doc_id = {}) ...".\
+          format(idx, doc_id)
+        )
+      # update posting (self.term_to_freq_pos)
+      for pos, term in enumerate(self.doc_tokens[doc_id]):
         if term in self.stop_list:
           continue
-        if (doc_id, term) not in term_to_freq_pos_dict:
-          term_to_freq_pos_dict[(doc_id, term)] = [0, []]
+        if (doc_id, term) not in self.term_to_freq_pos:
+          self.term_to_freq_pos[(doc_id, term)] = [0, []]
         # update freq and pos for current term
-        term_to_freq_pos_dict[(doc_id, term)][0] += 1
-        term_to_freq_pos_dict[(doc_id, term)][1].append(pos)
+        self.term_to_freq_pos[(doc_id, term)][0] += 1
+        self.term_to_freq_pos[(doc_id, term)][1].append(pos)
+    
+    # save self.term_to_freq_pos and self.doc_freq to disk
+    save_dict(self.term_to_freq_pos, "./data/term_to_freq_pos.pkl")
       
-      # turn the posting into a data.frame
-      self.posting_df = pd.DataFrame.from_dict(
-        term_to_freq_pos_dict, orient = 'index', 
-        columns = ["freq", "pos"]
-      )
-      docid_term = list(zip(*self.posting_df.index))
-      self.posting_df['doc_id'] = docid_term[0]
-      self.posting_df['term'] = docid_term[1]
-      self.posting_df = self.posting_df.reset_index(drop = True)\
-        [["doc_id", "term", "freq", "pos"]]\
-        .sort_values(by = "doc_id")
-      # create a dictionary base on posting
-      self.dict_df = self.posting_df.pivot_table(
-        index = 'term', values = ['doc_id', 'freq'],
-        aggfunc = {'doc_id': 'count', 'freq': np.sum}
-      ).reset_index()
+  @measure_time
+  def compute_doc_freq(self):
+    # turn the posting into a data.frame
+    posting_df = pd.DataFrame.from_dict(
+      self.term_to_freq_pos, orient = 'index', 
+      columns = ["freq", "pos"]
+    )
+    docid_term = list(zip(*posting_df.index))
+    posting_df['doc_id'] = docid_term[0]
+    posting_df['term'] = docid_term[1]
+    posting_df = posting_df.reset_index(drop = True)\
+      [["doc_id", "term", "freq", "pos"]]\
+      .sort_values(by = "doc_id")
+    
+    # create a dictionary base on posting_df
+    dictionary_df = posting_df.pivot_table(
+      index = 'term', values = ['doc_id', 'freq'],
+      aggfunc = {'doc_id': 'count', 'freq': np.sum}
+    ).reset_index()\
+    .rename(columns = {'doc_id': 'doc_freq', 'freq': 'total_freq'})
+    # update self.doc_freq
+    self.doc_freq = dict(
+      zip(dictionary_df['term'], dictionary_df['doc_freq'])
+    )
+
+  def bm25_score_term_doc(self, term, doc_id, k1 = 1.25, b = 0.75):
+    df_term = self.doc_freq[term]
+    tf_term_doc = self.term_to_freq_pos[(doc_id, term)][0]
+
+    score_idf = math.log((self.doc_count - df_term + 0.5) / (df_term + 0.5))
+    score_tf = ((k1 + 1) * tf_term_doc / 
+                (k1 * (1 - b + b * self.doc_length[doc_id] / 
+                self.avg_doc_length) + tf_term_doc))
+    return(score_idf * score_tf)
+  
+  def bm25_score(self, query, doc_id_list = None, k1 = 1.25, b = 0.75):
+    #! change `bm25_score` to general scoring
+    #! add a map from string to ranking functions
+    query_tokens = self.tokenize(query, remove_stop_words = True)
+    if doc_id_list is None: 
+      # if the user did not specify doc_id_list, will go through all documents
+      doc_id_list = self.doc_id
+    score_doc = [0] * len(doc_id_list)
+    for i, doc_id in enumerate(doc_id_list):
+      common_tokens = set(query_tokens) & set(self.doc_tokens[doc_id])
+      for term in common_tokens:
+        score_doc[i] += self.bm25_score_term_doc(term, doc_id) #! might want to specify k1 and b
+    return(score_doc)
+
+def get_retrieval_results(query, query_doc_id, num_results = 10):
+  doc_score =  indexes.bm25_score(query = query, doc_id_list = query_doc_id)
+  doc_score_df = pd.DataFrame(dict(doc_id = query_doc_id,score = doc_score)).\
+    sort_values(by = "score", ascending = False).\
+    reset_index(drop = True)
+  return(doc_score_df.loc[0:(num_results - 1), 'doc_id'].tolist())
 
 # -----------------------------------------------------------------------------
-documents = [
-   "Human machine interface, for lab abc computer applications",
-   "A computer survey of user opinion of computer system response time",
-   "The EPS user interface! management system",
-   "System and human system. engineering testing of EPS",
-   "Relation of user perceived response time to error measurement",
-   "The generation of random binary unordered trees",
-   "The intersection graph of, paths in trees",
-   "Graph minors IV Widths of trees and well quasi ordering",
-   "Graph minors A survey"
-]
-
 # import stop words
 with open('./lemur-stopwords.txt', 'r',
           encoding = "UTF-8") as f:
   stop_words = [line.strip() for line in f]
   f.close()
 
+# set up documents and doc_id
+documents = script_utterance.transcript.tolist()[:4001] #! update to all docs
+doc_id = script_utterance.u_id.tolist()[:4001] #! update to all docs
+
+# build inverted index
 indexes = Indexes(
   documents = documents, 
-  # documents = script_utterance.transcript.tolist(), 
+  doc_id = doc_id, 
   stop_words = stop_words,
-  stem = True
+  stem = False
 )
-indexes.generate_inverted_index()
-print(indexes.dict_df.head())
-print(indexes.posting_df.head(20))
+
+if __name__ == "__main__":
+  query_doc_id = doc_id
+  # query_doc_id = ['s01_e10_c02_u033', 's01_e10_c02_u042', 's01_e01_c01_u001']
+  print(get_retrieval_results(query = "you're going out with the guy", query_doc_id = query_doc_id))
