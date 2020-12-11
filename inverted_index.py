3983095364f38dc02e23725aa56360045125a62c
longyyu <longyyu@umich.edu>
2020-12-11 16:43:40 +0800

    Seperate measure_time, read_dict and save_dict from inverted_index.py to helper_func.py

--- inverted_index.py
+++ inverted_index.py
@@ -4,38 +4,14 @@ porter = PorterStemmer()
 import pandas as pd
 import numpy as np
 import math
-import time
-import pickle
 import os
 
+from helper_func import *
 from data_prep import script_utterance
 
 # Purpose: This script #! add header
 # Author: Yanyu Long
 # Updated: Dec 11, 2020
-
-# Define a decorator to measure the execution times of dedicated methods
-def measure_time(f):
-  def timed(*args, **kw):
-    time_start = time.time()
-    result = f(*args, **kw)
-    time_end = time.time()
-    print("Executed {} in {:2.2f} sec. ".format(
-      f.__name__, time_end - time_start
-    ))
-    return result
-  return timed
-
-@measure_time
-def read_dict(file_path):
-  with open(file_path, 'rb') as f:
-    obj = pickle.load(f)
-  return(obj)
-
-@measure_time
-def save_dict(obj, file_path):
-  with open(file_path, 'wb') as f:
-    pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 class Indexes:
   def __init__(self, documents, stop_words, doc_id = None, stem = False):
