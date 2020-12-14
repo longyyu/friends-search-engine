00200d6860410553a4c26c64fe2c1ceaa1c51b9e
longyyu <longyyu@umich.edu>
2020-12-14 23:36:13 +0800

    Finish ranker evaluation and parameter tuning

--- ranker_evaluation.py
+++ ranker_evaluation.py
@@ -2,12 +2,12 @@ import pandas as pd
 import numpy as np
 import math
 
-from data_prep import query_list, query_relevance
+from data_prep import query_list, query_relevance, uid_to_rowidx
 
 # Purpose: This script defines functions that are used to evaluate a 
 #          information retrieval model's performance. 
 # Author: Yanyu Long
-# Updated: Dec 13, 2020
+# Updated: Dec 14, 2020
 
 def calc_avg_precision(relevance_score):
   # input - relevance_score: an iterable object with numeric values
@@ -86,28 +86,148 @@ def evaluate_query_result(query_result):
 
 if __name__ == "__main__":
   NUM_RESULT = 10
-  
-  # retrieve documents using the baseline model
-  from metapy import metapy
-  from config_metapy import inv_idx, \
-    get_retrieval_results as get_retrieval_results_metapy
-  from data_prep import script_utterance, query_list
 
-  ranker =  metapy.index.OkapiBM25(k1 = 1.2, b = 0.75, k3 = 500)  
-  query_result = pd.DataFrame()
-  for q_id, query in enumerate(query_list):
-    query_result = query_result.append(
-      pd.DataFrame(dict(
-        query_id = q_id, 
-        doc_id = get_retrieval_results_metapy(
-          query, ranker, inv_idx, script_utterance, 
-          num_results = NUM_RESULT,
-          return_type = "row_idx"
-        )
-      )), 
-      ignore_index = True
+  # # evaluate the baseline model ----------------------------
+  # from metapy import metapy
+  # from config_metapy import inv_idx, \
+  #   get_retrieval_results as get_retrieval_results_metapy
+  # from data_prep import script_utterance, query_list
+  # ranker =  metapy.index.OkapiBM25(k1 = 1.2, b = 0.75, k3 = 500)  
+  # # retrieve documents
+  # query_result = pd.DataFrame()
+  # for q_id, query in enumerate(query_list):
+  #   query_result = query_result.append(
+  #     pd.DataFrame(dict(
+  #       query_id = q_id, 
+  #       doc_id = get_retrieval_results_metapy(
+  #         query, ranker, inv_idx, script_utterance, 
+  #         num_results = NUM_RESULT,
+  #         return_type = "row_idx"
+  #       )
+  #     )), 
+  #     ignore_index = True
+  #   )
+  # # evaluate ranker performance
+  # baseline_eval = evaluate_query_result(query_result)
+  # print(baseline_eval)
+  # print(baseline_eval[["ap", "ndcg"]].mean())
+
+  # evaluate other ranking functions ---------------------------
+  from inverted_index import get_retrieval_results
+  from itertools import chain
+
+  def evaluate_ranker(ranker, **kwargs):
+    # given ranker name and parameters (specified by **kwargs),
+    # evaluate the ranker's performance on each testing query using AP and 
+    # NDCG, and returns a data frame containing the evaluation results
+    
+    # retrieve documents
+    query_result = pd.DataFrame()
+    for q_id, query in enumerate(query_list):
+      query_result = query_result.append(
+        pd.DataFrame(dict(
+          query_id = q_id, 
+          doc_id = get_retrieval_results(
+            query = query, ranker = ranker, num_results = NUM_RESULT, **kwargs
+          )
+        )), 
+        ignore_index = True
+      )
+    # transform utterance ID into document row index
+    query_result['doc_id'] = query_result['doc_id'].apply(
+      lambda x: uid_to_rowidx[x]
     )
-  # evaluate ranker performance
-  baseline_eval = evaluate_query_result(query_result)
-  print(baseline_eval)
-  print(baseline_eval[["ap", "ndcg"]].mean())
+    # evaluate ranker performance
+    ranker_eval = evaluate_query_result(query_result)
+    ranker_eval_avg = dict(ranker_eval[["ap", "ndcg"]].mean())
+    # add ranker and parameters to the dictionary
+    params = ', '.join([
+      "{}={}".format(key, val) for key, val in kwargs.items()
+    ])
+    ranker_eval_avg.update(dict(ranker = ranker, params = params))
+    return(pd.DataFrame.from_records(ranker_eval_avg, index = pd.Index([0])))
+  
+  # # bm25 -------------------------------------------------------------------
+  # k1_val = np.arange(0.4, 2.0, 0.2).tolist()
+  # b_val = np.arange(0.6, 0.9, 0.05).tolist()
+  # rankers = pd.DataFrame(dict(
+  #   ranker = "bm25",
+  #   k1 = list(chain.from_iterable(
+  #     [[val] * len(b_val) for val in k1_val]
+  #   )),
+  #   b = b_val * len(k1_val)
+  # ))
+  # rankers_eval = pd.concat(
+  #   list(rankers.apply(
+  #     lambda row: evaluate_ranker(
+  #       ranker = row["ranker"], k1 = row["k1"], b = row["b"]
+  #     ), axis = 1
+  #   )), 
+  #   ignore_index = True
+  # )
+  # rankers_eval[["ranker", "params", "ap", "ndcg"]].to_csv(
+  #   "./data/ranker_evaluation_bm25.csv", index = False
+  # )
+  # print(rankers_eval.sort_values(by = "ap", ascending = False).head(10))
+
+  # # bm25_v1 ---------------------------------------------------------------
+  # k1_val = np.arange(0.4, 2.0, 0.2).tolist()
+  # b_val = np.arange(0.6, 0.9, 0.05).tolist()
+  # rankers = pd.DataFrame(dict(
+  #   ranker = "bm25_v1",
+  #   k1 = list(chain.from_iterable(
+  #     [[val] * len(b_val) for val in k1_val]
+  #   )),
+  #   b = b_val * len(k1_val)
+  # ))
+  # rankers_eval = pd.concat(
+  #   list(rankers.apply(
+  #     lambda row: evaluate_ranker(
+  #       ranker = row["ranker"], k1 = row["k1"], b = row["b"]
+  #     ), axis = 1
+  #   )), 
+  #   ignore_index = True
+  # )
+  # rankers_eval[["ranker", "params", "ap", "ndcg"]].to_csv(
+  #   "./data/ranker_evaluation_bm25_v1.csv", index = False
+  # )
+  # print(rankers_eval.sort_values(by = "ap", ascending = False).head(10))
+
+  # # pivoted length ------------------------------------------------------
+  # rankers = pd.DataFrame(dict(
+  #   ranker = "piv", b = np.arange(0.05, 1.00, 0.05).tolist()
+  # ))
+  # rankers_eval = pd.concat(
+  #   list(rankers.apply(
+  #     lambda row: evaluate_ranker(
+  #       ranker = row["ranker"], b = row["b"]
+  #     ), axis = 1
+  #   )), 
+  #   ignore_index = True
+  # )
+  # rankers_eval[["ranker", "params", "ap", "ndcg"]].to_csv(
+  #   "./data/ranker_evaluation_piv.csv", index = False
+  # )
+  # print(rankers_eval.sort_values(by = "ap", ascending = False).head(10))
+
+  # # tsl ---------------------------------------------------------------
+  # rankers = pd.DataFrame(dict(
+  #   ranker = "tsl", mu = np.arange(2200, 4200, 200).tolist()
+  # ))
+  # rankers_eval = pd.concat(
+  #   list(rankers.apply(
+  #     lambda row: evaluate_ranker(
+  #       ranker = row["ranker"], mu = row["mu"]
+  #     ), axis = 1
+  #   )), 
+  #   ignore_index = True
+  # )
+  # rankers_eval[["ranker", "params", "ap", "ndcg"]].to_csv(
+  #   "./data/ranker_evaluation_tsl.csv", index = False
+  # )
+  # print(rankers_eval.sort_values(by = "ap", ascending = False).head(10))
+
+  # others ---------------------------------------------------------
+  print(evaluate_ranker(ranker = "es"))
+  print(evaluate_ranker(ranker = "f2exp"))
+  # print(evaluate_ranker(ranker = "bm25", k1 = 1.6, b = 0.8)) # best model
